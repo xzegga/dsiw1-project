87dbc0e9da3e1dfa5fbca132d6b416d0e0f73cb1
Raúl Escamilla <raul.escamilla@asesoriait.com>
2024-04-05 21:01:08 -0600

    Custom configuration manager added

--- WCFMiCafesitoServices/App_Code/Service.cs
+++ WCFMiCafesitoServices/App_Code/Service.cs
@@ -1,29 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
-// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
 public class Service : IService
 {
-	public string GetData(int value)
-	{
-		return string.Format("You entered: {0}", value);
-	}
+    private CustomConfigurationManager _config;
 
-	public CompositeType GetDataUsingDataContract(CompositeType composite)
-	{
-		if (composite == null)
-		{
-			throw new ArgumentNullException("composite");
-		}
-		if (composite.BoolValue)
-		{
-			composite.StringValue += "Suffix";
-		}
-		return composite;
-	}
+    public Service()
+    {
+        _config = new CustomConfigurationManager();
+        string c = _config.ConnectionString;
+    }
+
+    public void GetData()
+    {
+        string c = _config.ConnectionString;
+    }
+
+    public CompositeType GetDataUsingDataContract(CompositeType composite)
+    {
+        throw new NotImplementedException();
+    }
 }
