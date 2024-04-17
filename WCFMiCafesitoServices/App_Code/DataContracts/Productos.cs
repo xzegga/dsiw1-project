9dfdbe9e917cd7864dc5215eca28d6695a5ae80c
Kim0433 <167261262+Kim0433@users.noreply.github.com>
2024-04-16 21:24:35 -0600

    Update Productos.cs

--- WCFMiCafesitoServices/App_Code/DataContracts/Productos.cs
+++ WCFMiCafesitoServices/App_Code/DataContracts/Productos.cs
@@ -1,1 +1,24 @@
 
+using System;
+using System.Runtime.Serialization;
+
+[DataContract]
+public class Category
+{
+
+    [DataMember]
+    public int ID_Producto { get; set; }
+
+    [DataMember]
+    public string Nombre { get; set; }
+
+    [DataMember]
+    public string Descripcion { get; set; }
+
+    [DataMember]
+    public string Precio { get; set; }
+
+    [DataMember]
+    public string ID_Categoria { get; set; }
+
+}
