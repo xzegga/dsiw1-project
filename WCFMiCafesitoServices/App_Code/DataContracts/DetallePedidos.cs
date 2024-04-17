27579cb5491d6992bcf0a9496588d597a10245da
Kim0433 <167261262+Kim0433@users.noreply.github.com>
2024-04-16 22:13:11 -0600

    Update DetallePedidos.cs

--- WCFMiCafesitoServices/App_Code/DataContracts/DetallePedidos.cs
+++ WCFMiCafesitoServices/App_Code/DataContracts/DetallePedidos.cs
@@ -1,1 +1,33 @@
+using System;
+using System.Runtime.Serialization;
+
+[DataContract]
+public class DetalleService
+{
+
+    [DataMember]
+    public int ID_Detalle { get; set; }
+
+    [DataMember]
+    public string ID_Pedido { get; set; }
+
+    [DataMember]
+    public string ID_Producto { get; set; }
+
+    [DataMember]
+    public string Cantidad { get; set; }
+
+    [DataMember]
+    public string PrecioUnitario { get; set; }
+
+    [DataMember]
+    public string Nombre { get; set; }
+
+    [DataMember]
+    public string Descripcion { get; set; }
+
+    [DataMember]
+    public string Precio { get; set; }
+
+}
 
