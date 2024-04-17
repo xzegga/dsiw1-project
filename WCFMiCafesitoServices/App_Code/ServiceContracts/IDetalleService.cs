865b31ded2314c622958cbbd7e584b81710b0d47
Kim0433 <167261262+Kim0433@users.noreply.github.com>
2024-04-16 21:12:25 -0600

    Update IDetalleService.cs

--- WCFMiCafesitoServices/App_Code/ServiceContracts/IDetalleService.cs
+++ WCFMiCafesitoServices/App_Code/ServiceContracts/IDetalleService.cs
@@ -1,1 +1,21 @@
+using System.Collections.Generic;
+using System.ServiceModel;
 
+namespace MiCafesito
+{
+    [ServiceContract]
+    public interface IDetalleService
+    {
+        [OperationContract]
+        List<DetallePedidos> GetAllDetPedido();
+
+        [OperationContract]
+        void UpdateDetPedido(DetallePedido Detallepedido);
+
+        [OperationContract]
+        void DeleteDetPedido(int id);
+
+        [OperationContract]
+        void AddDetPedido(DetallePedido Detallepedido);
+    }
+}
