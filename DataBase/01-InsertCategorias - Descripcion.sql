e10480d710eceffe2f313fbcea74ee2913b6a3f7
Raúl Escamilla <raul.escamilla@asesoriait.com>
2024-05-21 20:04:43 -0600

    Folder renamed

---
+++ DataBase/01-InsertCategorias - Descripcion.sql
@@ -0,0 +1,9 @@
+-- Actualizando la descripción de cada categoría
+UPDATE [dbo].[Categorias] SET [Descripcion] = 'Productos relacionados con filtros para café.' WHERE [ID_Categoria] = 1;
+UPDATE [dbo].[Categorias] SET [Descripcion] = 'Equipos para preparar café espresso.' WHERE [ID_Categoria] = 3;
+UPDATE [dbo].[Categorias] SET [Descripcion] = 'Molinillos eléctricos y manuales para moler granos de café.' WHERE [ID_Categoria] = 4;
+UPDATE [dbo].[Categorias] SET [Descripcion] = 'Métodos de preparación directa de café, como prensa francesa, aeropress y moka pot.' WHERE [ID_Categoria] = 5;
+UPDATE [dbo].[Categorias] SET [Descripcion] = 'Métodos de filtrado de café, como Chemex, V60, Kalita Wave y Clever Dripper.' WHERE [ID_Categoria] = 6;
+UPDATE [dbo].[Categorias] SET [Descripcion] = 'Otros métodos especiales de preparación de café, como sifón de vacío.' WHERE [ID_Categoria] = 7;
+UPDATE [dbo].[Categorias] SET [Descripcion] = 'Accesorios para la preparación y consumo de café, como portafiltro, tamper, jarra de leche, etc.' WHERE [ID_Categoria] = 8;
+UPDATE [dbo].[Categorias] SET [Descripcion] = 'Insumos relacionados con la preparación y mantenimiento de equipos de café, como filtros de repuesto y productos de limpieza.' WHERE [ID_Categoria] = 9;
--- Scripts/InsertProductos.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO [dbo].[Categorias] ([Nombre], [Descripcion]) VALUES ('Cafeteras de Espresso', NULL);
-INSERT INTO [dbo].[Categorias] ([Nombre], [Descripcion]) VALUES ('Molinillos de Café', NULL);
-INSERT INTO [dbo].[Categorias] ([Nombre], [Descripcion]) VALUES ('Métodos de Infusión Directa', NULL);
-INSERT INTO [dbo].[Categorias] ([Nombre], [Descripcion]) VALUES ('Métodos de Filtrado', NULL);
-INSERT INTO [dbo].[Categorias] ([Nombre], [Descripcion]) VALUES ('Otros Métodos Especiales', NULL);
-INSERT INTO [dbo].[Categorias] ([Nombre], [Descripcion]) VALUES ('Accesorios', NULL);
-INSERT INTO [dbo].[Categorias] ([Nombre], [Descripcion]) VALUES ('Insumos', NULL);
